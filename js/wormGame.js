9ab2ef48f016714a3b7350e43d52c8d3028b3076
Maribel Obando Joven <maribel-obando.j@utp.edu.co>
2023-09-11 17:58:00 -0500

    generador de steps y movimiento fichas

--- js/wormGame.js
+++ js/wormGame.js
@@ -1,47 +1,129 @@
+let turn = 1;
+let cont = 0;
+let palabrasTospell = ['ROUGH', 'JOKE', 'STRAWBERRY', 'WATERMELON', 'NAPKIN', 'SHELF'];
+let spelling = ['AR - OU - IU - YI - EICH', 'YEI - OU - KEI - I'];
+let jugadorImpar = 0;
+let jugadorPar = 0;
+
 function comenzar(){
     let numJugadores = document.getElementById('numPlayers').value;
     document.getElementById('numPlayers').value = "Zero";
 
     let iconoPawn;
     let nodoPawn ;
-    let lugar;
+    let lugar = 'a';
 
-    for(let i = 1 ; i <= 4; i++){
-        lugar = i.toString();    
+    for(let i = 1 ; i <= 2; i++){
+
         nodoPawn = document.getElementById(lugar);
+        lugar = 'b';
         nodoPawn.innerHTML = "";
     }
     
 
     if(numJugadores != 0){
+        lugar = 'a';
         for(let i = 1 ; i <= parseInt(numJugadores); i++){
             iconoPawn = document.createElement('i');
-            iconoPawn.classList.add("fa-solid", "fa-chess-pawn", "fa-2xl");   
-            
-            lugar = i.toString();    
+            iconoPawn.classList.add("fa-solid", "fa-chess-pawn", "fa-2xl");
+             
             nodoPawn = document.getElementById(lugar);
             
             if(i == 1){
                 iconoPawn.classList.add("text-info");
+                lugar = 'b';
             }else if(i == 2){
                 iconoPawn.classList.add("text-success");
-            }else if(i == 3){
-                iconoPawn.classList.add("text-primary");
-            }else if(i == 4){
-                iconoPawn.classList.add("text-warning");
             }
             
             nodoPawn.appendChild(iconoPawn);
 
         }
+
     }
 }
 
-let cont = 0;
-let palabrasTospell = ['ROUGH', 'JOKE', 'STRAWBERRY', 'WATERMELON', 'NAPKIN', 'SHELF'];
-let spelling = ['AR - OU - IU - YI - EICH', 'YEI - OU - KEI - I'];
+function getSteps(){
+
+    let steps = Math.floor(Math.random() * 6) + 1;
+    let numEscogido = document.getElementById('randomNum');
+    let oldDie = document.getElementById('dadoVisto');
+
+    let newDie = document.createElement('i');
+
+    if(steps == 1){
+            
+        newDie.classList.add("fa-solid", "fa-dice-one", "fs-1", "pb-2");
+        newDie.id = 'dadoVisto';
+
+        numEscogido.replaceChild(newDie, oldDie);
+    }else if(steps == 2){
+        newDie.classList.add("fa-solid", "fa-dice-two" ,"fs-1", "pb-2");
+        newDie.id = 'dadoVisto';
+
+        numEscogido.replaceChild(newDie, oldDie);
+    }else if(steps == 3){
+        newDie.classList.add("fa-solid", "fa-dice-three" ,"fs-1", "pb-2");
+        newDie.id = 'dadoVisto';
+
+        numEscogido.replaceChild(newDie, oldDie);
+    }else if(steps == 4){
+        newDie.classList.add("fa-solid", "fa-dice-four" ,"fs-1", "pb-2");
+        newDie.id = 'dadoVisto';
+
+        numEscogido.replaceChild(newDie, oldDie);
+    }else if(steps == 5){
+        newDie.classList.add("fa-solid", "fa-dice-five" ,"fs-1", "pb-2");
+        newDie.id = 'dadoVisto';
+
+        numEscogido.replaceChild(newDie, oldDie);
+    }else if(steps == 6){
+        newDie.classList.add("fa-solid", "fa-dice-six" ,"fs-1", "pb-2");
+        newDie.id = 'dadoVisto';
+
+        numEscogido.replaceChild(newDie, oldDie);
+    }
+
+    if(turn % 2 == 0){
+        newDie.classList.add("text-success");
+    }else{
+        newDie.classList.add("text-info");
+    }
+
+    turn++;   
+    move(steps);
+}
+
+function move(steps){
+    cont++;
+        
+    if(cont % 2 == 0){
+        jugadorPar += steps;
+        let casilla = document.getElementById(""+jugadorPar+"");
+        let iconoPawn = document.createElement('i');
+        iconoPawn.id = cont;
+        iconoPawn.classList.add("fa-solid", "fa-chess-pawn", "fa-2xl", "text-success");
+        casilla.appendChild(iconoPawn);
+        document.getElementById("b").innerHTML = "";
+
+        
+        
+    }else{
+        jugadorImpar += steps;
+        let casilla = document.getElementById(""+jugadorImpar+"");
+        let iconoPawn = document.createElement('i');
+        iconoPawn.classList.add("fa-solid", "fa-chess-pawn", "fa-2xl", "text-info");
+        casilla.appendChild(iconoPawn);
+        document.getElementById("a").innerHTML = "";
+
+    }
+}
+
 
 function drill(clue){
+
+    
+
     if(clue == 'a'){
         let ejercicio = document.createElement('div');
         ejercicio.id = 'ejercicioSpelling';
